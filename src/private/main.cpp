c12bde9ba812d56f231c45e02a60a8537e346afa
Game.Play() <gamedotplay0@gmail.com>
2023-09-15 13:23:01 -0400

    Update main.cpp
    Updated some error messages.
    Added measuring the runtime of the GaussianBlue() effect.

--- src/private/main.cpp
+++ src/private/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <cstdint>
-#include <vector>
 #include <string>
-#include <fstream>
+#include <chrono>
 #include "../TGA/public/TgaImage.h"
 #include "../public/Effects.h"
 
@@ -11,7 +9,7 @@ int main(int argc, char** argv)
 	if (argc != 4)
 	{
 		std::cout << "Incorrect parameters. Correct usage is:" << std::endl;
-		std::cout << ".>ImageManipulation.exe <path to original image> <path to newly saved image> <blur amount 0.0 - 1.0>" << std::endl;
+		std::cout << ".>ImageManipulation.exe <Input Image Path> <Output Image Path> <Blur Strength 0-1>" << std::endl;
 		return -1;
 	}
 
@@ -20,6 +18,14 @@ int main(int argc, char** argv)
 	float blurValue = std::stof(argv[3]);
 
 	TgaImage tgaImage(inputPath);
+
+	if (tgaImage.GetHeader() == nullptr)
+	{
+		std::cout << "An error occurred while attempting to parse image " << inputPath << std::endl;
+		std::cout << "Verify correct image path or try a different image." << std::endl;
+		return -1;
+	}
+
 	if ((int)tgaImage.GetHeader()->ImageType != 2)
 	{
 		std::cout << "This image is of ImageType=" << (int)tgaImage.GetHeader()->ImageType << std::endl;
@@ -27,7 +33,14 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	auto start = std::chrono::high_resolution_clock::now();
 	Effects::GaussianBlur(tgaImage, blurValue);
+	auto stop = std::chrono::high_resolution_clock::now();
+
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
 	tgaImage.SaveToFile(outputPath);
+
+	std::cout << "New image saved to " << outputPath << std::endl;
+	std::cout << "Gaussian Blur runtime: " << duration.count() << "ms";
 }
