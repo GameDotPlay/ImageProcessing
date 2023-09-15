74b70b6c0e0cae0f9b6df81c8e425f51cc306adb
Game.Play() <gamedotplay0@gmail.com>
2023-09-14 22:35:04 -0400

    Update Effects.cpp
    Tweaking on Gaussian Blur algorithm.

--- src/private/Effects.cpp
+++ src/private/Effects.cpp
@@ -6,34 +6,72 @@
 
 void Effects::GaussianBlur(TgaImage& tgaImage, float blurAmount)
 {
-	if (blurAmount == 0.0f)
-	{
-		return;
-	}
+	blurAmount = std::clamp(blurAmount, 0.0f, 1.0f);
 
-	// Clamp blur amount to between 0 and 1.
-	blurAmount = blurAmount < 0.0f ? 0.0f : blurAmount;
-	blurAmount = blurAmount > 1.0f ? 1.0f : blurAmount;
-
-	// Copy the image pixel data to store new pixel values.
+	// Create pixel data to store new pixel values;
 	std::vector<Pixel> newPixels(tgaImage.GetPixelData().size());
 
-	int radius = std::max((int)(10 * blurAmount), 1);
-	int kernelWidth = (2 * radius) + 1;
-	float sigma = (float)radius / 2.0f;
-	std::vector<std::vector<float> > kernel(kernelWidth, std::vector<float>(kernelWidth));
-
-	// Calculate Gaussian values for the kernel matrix.
-	float sum = 0.0f;
-	for (int32_t x = -radius; x <= radius; x++)
+	float sigma = 5.0f * blurAmount;
+	int32_t kernelWidth = std::max((int)round(2 * M_PI * sigma * blurAmount), 3);
+	if (kernelWidth % 2 == 0)
 	{
-		for (int32_t y = -radius; y <= radius; y++)
+		kernelWidth--;
+	}
+
+	int32_t radius = kernelWidth / 2;
+
+	std::vector<std::vector<float>> kernel = Effects::GetGaussianMatrix(radius, sigma, kernelWidth);
+
+	// Apply filter to each pixel in the image.
+	for (size_t pixelIndex = 0; pixelIndex < tgaImage.GetPixelData().size(); pixelIndex++)
+	{
+		float red = 0.0f;
+		float green = 0.0f;
+		float blue = 0.0f;
+
+		for (int32_t kernelY = -radius; kernelY <= radius; kernelY++)
 		{
-			float exponentNumerator = -((x * x) + (y * y));
-			float exponentDenominator = 2 * (sigma * sigma);
+			for (int32_t kernelX = -radius; kernelX <= radius; kernelX++)
+			{
+				float kernelValue = kernel[kernelY + radius][kernelX + radius];
+				int32_t kernelPixelIndex = (pixelIndex + kernelX) + (tgaImage.GetHeader()->Width * kernelY);
+
+				// If the kernel tries to process a pixel outside of the image, just continue.
+				if (kernelPixelIndex < 0 || kernelPixelIndex > tgaImage.GetPixelData().size() - 1)
+				{
+					continue;
+				}
+
+				red += (float)tgaImage.GetPixelData()[kernelPixelIndex].red * kernelValue;
+				green += (float)tgaImage.GetPixelData()[kernelPixelIndex].green * kernelValue;
+				blue += (float)tgaImage.GetPixelData()[kernelPixelIndex].blue * kernelValue;
+			}
+		}
+
+		newPixels[pixelIndex].red = round(red);
+		newPixels[pixelIndex].green = round(green);
+		newPixels[pixelIndex].blue = round(blue);
+		newPixels[pixelIndex].alpha = tgaImage.GetPixelData()[pixelIndex].alpha;
+	}
+
+	// Replace the old pixel data with the new pixel data.
+	tgaImage.SetPixelData(newPixels);
+}
+
+std::vector<std::vector<float>> Effects::GetGaussianMatrix(const int32_t radius, const float sigma, const size_t kernelWidth)
+{
+	// Calculate Gaussian values for the kernel matrix.
+	std::vector<std::vector<float>> kernel(kernelWidth, std::vector<float>(kernelWidth));
+	float sum = 0.0f;
+	for (int32_t y = -radius; y <= radius; y++)
+	{
+		for (int32_t x = -radius; x <= radius; x++)
+		{
+			float exponentNumerator = -(float)((x * x) + (y * y));
+			float exponentDenominator = 2.0f * (sigma * sigma);
 
 			float eExpression = exp(exponentNumerator / exponentDenominator);
-			float kernelValue = (eExpression / (2 * M_PI * (sigma * sigma)));
+			float kernelValue = eExpression / (2.0f * (float)M_PI * (sigma * sigma));
 
 			kernel[x + radius][y + radius] = kernelValue;
 			sum += kernelValue;
@@ -41,120 +79,13 @@ void Effects::GaussianBlur(TgaImage& tgaImage, float blurAmount)
 	}
 
 	// Normalize kernel values to between 0 and 1.
-	for (size_t x = 0; x < kernelWidth; x++)
+	for (size_t y = 0; y < kernelWidth; y++)
 	{
-		for (size_t y = 0; y < kernelWidth; y++)
+		for (size_t x = 0; x < kernelWidth; x++)
 		{
 			kernel[x][y] /= sum;
 		}
 	}
 
-	// Apply filter to each pixel in the image.
-	for (size_t pixelIndex = 0; pixelIndex < tgaImage.GetPixelData().size(); pixelIndex++)
-	{
-		uint8_t red = 0;
-		uint8_t green = 0;
-		uint8_t blue = 0;
-
-		for (int32_t kernelX = -radius; kernelX <= radius; kernelX++)
-		{
-			for (int32_t kernelY = -radius; kernelY <= radius; kernelY++)
-			{
-				float kernelValue = kernel[kernelX + radius][kernelY + radius];
-				int32_t kernelPixelIndex = (pixelIndex + kernelX) + (tgaImage.GetHeader()->Width * kernelY);
-
-				// If the kernel tries to process a pixel outside of the image, find the nearest valid pixel and use that value.
-				if (kernelPixelIndex < 0)
-				{
-					kernelPixelIndex += kernelPixelIndex;
-
-					if (kernelX < 0)
-					{
-						if (kernelY < 0)
-						{
-							red += (tgaImage.GetPixelData()[kernelPixelIndex].red) * kernelValue;
-							green += (tgaImage.GetPixelData()[kernelPixelIndex].green) * kernelValue;
-							blue += (tgaImage.GetPixelData()[kernelPixelIndex].blue) * kernelValue;
-						}
-						else
-						{
-							kernelPixelIndex += (tgaImage.GetHeader()->Width * kernelY);
-							red += (tgaImage.GetPixelData()[kernelPixelIndex].red) * kernelValue;
-							green += (tgaImage.GetPixelData()[kernelPixelIndex].green) * kernelValue;
-							blue += (tgaImage.GetPixelData()[kernelPixelIndex].blue) * kernelValue;
-						}
-					}
-					else if (kernelY < 0)
-					{
-						if (kernelX < 0)
-						{
-							red += (tgaImage.GetPixelData()[kernelPixelIndex].red) * kernelValue;
-							green += (tgaImage.GetPixelData()[kernelPixelIndex].green) * kernelValue;
-							blue += (tgaImage.GetPixelData()[kernelPixelIndex].blue) * kernelValue;
-						}
-						else
-						{
-							kernelPixelIndex += kernelX;
-							red += (tgaImage.GetPixelData()[kernelPixelIndex].red) * kernelValue;
-							green += (tgaImage.GetPixelData()[kernelPixelIndex].green) * kernelValue;
-							blue += (tgaImage.GetPixelData()[kernelPixelIndex].blue) * kernelValue;
-						}
-					}
-
-					continue;
-				}
-				else if (kernelPixelIndex > tgaImage.GetPixelData().size() - 1)
-				{
-					kernelPixelIndex = tgaImage.GetPixelData().size() - 1;
-
-					if (kernelX > 0)
-					{
-						if (kernelY > 0)
-						{
-							red += (tgaImage.GetPixelData()[kernelPixelIndex].red) * kernelValue;
-							green += (tgaImage.GetPixelData()[kernelPixelIndex].green) * kernelValue;
-							blue += (tgaImage.GetPixelData()[kernelPixelIndex].blue) * kernelValue;
-						}
-						else
-						{
-							kernelPixelIndex -= (tgaImage.GetHeader()->Width * kernelY);
-							red += (tgaImage.GetPixelData()[kernelPixelIndex].red) * kernelValue;
-							green += (tgaImage.GetPixelData()[kernelPixelIndex].green) * kernelValue;
-							blue += (tgaImage.GetPixelData()[kernelPixelIndex].blue) * kernelValue;
-						}
-					}
-					else if (kernelY > 0)
-					{
-						if (kernelX > 0)
-						{
-							red += (tgaImage.GetPixelData()[kernelPixelIndex].red) * kernelValue;
-							green += (tgaImage.GetPixelData()[kernelPixelIndex].green) * kernelValue;
-							blue += (tgaImage.GetPixelData()[kernelPixelIndex].blue) * kernelValue;
-						}
-						else
-						{
-							kernelPixelIndex -= kernelX;
-							red += (tgaImage.GetPixelData()[kernelPixelIndex].red) * kernelValue;
-							green += (tgaImage.GetPixelData()[kernelPixelIndex].green) * kernelValue;
-							blue += (tgaImage.GetPixelData()[kernelPixelIndex].blue) * kernelValue;
-						}
-					}
-				}
-				else
-				{
-					red += (tgaImage.GetPixelData()[kernelPixelIndex].red) * kernelValue;
-					green += (tgaImage.GetPixelData()[kernelPixelIndex].green) * kernelValue;
-					blue += (tgaImage.GetPixelData()[kernelPixelIndex].blue) * kernelValue;
-				}
-			}
-		}
-
-		newPixels[pixelIndex].red = red;
-		newPixels[pixelIndex].green = green;
-		newPixels[pixelIndex].blue = blue;
-		newPixels[pixelIndex].alpha = tgaImage.GetPixelData()[pixelIndex].alpha;
-	}
-
-	// Replace the old pixel data with the new pixel data.
-	tgaImage.SetPixelData(newPixels);
+	return kernel;
 }
