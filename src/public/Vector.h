a329511668bd56ded8e56be8f6cfedec46bf0a8d
Game.Play() <gamedotplay0@gmail.com>
2023-09-27 19:28:01 -0400

    Renamed file to Vector.h and added more structs
    Added Vec2, Vec3, Vec4, Vec2f, Vec3f, Vec4f structs.

---
+++ src/public/Vector.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <cstdint>
+
+struct Vec2
+{
+	uint8_t x;
+	uint8_t y;
+};
+
+struct Vec3
+{
+	uint8_t x;
+	uint8_t y;
+	uint8_t z;
+};
+
+struct Vec4
+{
+	uint8_t x;
+	uint8_t y;
+	uint8_t z;
+	uint8_t w;
+};
+
+struct Vec2f
+{
+	float x;
+	float y;
+};
+
+struct Vec3f
+{
+	float x;
+	float y;
+	float z;
+};
+
+struct Vec4f
+{
+	float x;
+	float y;
+	float z;
+	float w;
+};
