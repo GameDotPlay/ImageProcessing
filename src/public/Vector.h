18c7f8947a3b90c05c917eed90d6485e11872577
Game.Play() <gamedotplay0@gmail.com>
2023-10-04 16:50:10 -0400

    Update Vector.h
    Added overloaded operator== to all structs.
    Added hash specialization for Vec4 so I can use it as a key in <unordered_map>.

--- src/public/Vector.h
+++ src/public/Vector.h
@@ -1,10 +1,18 @@
 #pragma once
+
 #include <cstdint>
+#include <functional>
 
 struct Vec2
 {
 	uint8_t x;
 	uint8_t y;
+
+	bool operator==(const Vec2& other) const
+	{
+		return (x == other.x
+			&& y == other.y);
+	}
 };
 
 struct Vec3
@@ -12,6 +20,13 @@ struct Vec3
 	uint8_t x;
 	uint8_t y;
 	uint8_t z;
+
+	bool operator==(const Vec3& other) const
+	{
+		return (x == other.x
+			&& y == other.y
+			&& z == other.z);
+	}
 };
 
 struct Vec4
@@ -20,6 +35,14 @@ struct Vec4
 	uint8_t y;
 	uint8_t z;
 	uint8_t w;
+
+	bool operator==(const Vec4& other) const
+	{
+		return (x == other.x
+			&& y == other.y
+			&& z == other.z
+			&& w == other.w);
+	}
 };
 
 struct Vec2f
@@ -42,3 +65,18 @@ struct Vec4f
 	float z;
 	float w;
 };
+
+namespace std
+{
+	template<> struct hash<Vec4>
+	{
+		size_t operator()(const Vec4& k) const
+		{
+			return ((hash<uint8_t>()(k.x)
+				^ (hash<uint8_t>()(k.y) << 1)) >> 1)
+				^ (hash<uint8_t>()(k.z) << 1)
+				^ (hash<uint8_t>()(k.w) << 1);
+		}
+	};
+}
+
