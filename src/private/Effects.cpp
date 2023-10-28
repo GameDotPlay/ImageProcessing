c5d4736ea1e49a3d2cb7de60fd3c3a7b211e4d4e
Game.Play() <gamedotplay0@gmail.com>
2023-10-28 08:59:41 -0400

    Optimized the blur algorithm to perform the operation on one axis at a time.

--- src/private/Effects.cpp
+++ src/private/Effects.cpp
@@ -25,10 +25,7 @@ std::shared_ptr<Vec4[]> const Effects::GaussianBlur(const std::shared_ptr<Vec4[]
 	// Apply filter to each pixel in the image.
 	for (size_t pixelIndex = 0; pixelIndex < length; pixelIndex++)
 	{
-		float red = 0.0f;
-		float green = 0.0f;
-		float blue = 0.0f;
-		float alpha = 0.0f;
+		Vec4f newPixel = {};
 
 		for (int32_t kernelY = -radius; kernelY <= radius; kernelY++)
 		{
@@ -43,17 +40,101 @@ std::shared_ptr<Vec4[]> const Effects::GaussianBlur(const std::shared_ptr<Vec4[]
 					continue;
 				}
 
-				red += (float)pixels[kernelSamplePixelIndex].x * kernelValue;
-				green += (float)pixels[kernelSamplePixelIndex].y * kernelValue;
-				blue += (float)pixels[kernelSamplePixelIndex].z * kernelValue;
-				alpha += (float)pixels[kernelSamplePixelIndex].w * kernelValue;
+				newPixel.x += (float)pixels[kernelSamplePixelIndex].x * kernelValue;
+				newPixel.y += (float)pixels[kernelSamplePixelIndex].y * kernelValue;
+				newPixel.z += (float)pixels[kernelSamplePixelIndex].z * kernelValue;
+				newPixel.w += (float)pixels[kernelSamplePixelIndex].w * kernelValue;
 			}
 		}
 
-		newPixels[pixelIndex].x = round(red);
-		newPixels[pixelIndex].y = round(green);
-		newPixels[pixelIndex].z = round(blue);
-		newPixels[pixelIndex].w = round(alpha);
+		newPixels[pixelIndex].x = round(newPixel.x);
+		newPixels[pixelIndex].y = round(newPixel.y);
+		newPixels[pixelIndex].z = round(newPixel.z);
+		newPixels[pixelIndex].w = round(newPixel.w);
+	}
+
+	return newPixels;
+}
+
+std::shared_ptr<Vec4[]> const Effects::GaussianBlurSeparate(const std::shared_ptr<Vec4[]>& pixels, const size_t width, const size_t height, float blurAmount)
+{
+	blurAmount = std::clamp(blurAmount, 0.0f, 1.0f);
+
+	// Create pixel data to store new pixel values;
+	size_t length = width * height;
+	std::shared_ptr<Vec4[]> newPixels = std::make_shared<Vec4[]>(length);
+
+	// Scale the radius of the blurring effect by blurAmount, but we always want a radius of at least 1.
+	// A value of 10 is chosen here as a reasonable maximum value of the radius to get a near-unrecognizable image at blurAmount = 1.
+	int32_t radius = std::max((int)round(10 * blurAmount), 1);
+
+	// Scale the sigma value by the blurAmount, but we always want a sigma of at least 1.
+	// A value of 10 is chosen here as a reasonable maximum value for sigma to get a near-unrecognizable image at blurAmount = 1
+	float sigma = std::max(10.0f * blurAmount, 1.0f);
+
+	std::vector<float> kernel = Effects::Get1DMatrix(radius, sigma);
+
+	// Apply a 1D kernel in the horizontal orientation to all pixels.
+	for (size_t pixelIndex = 0; pixelIndex < length; pixelIndex++)
+	{
+		Vec4f pixel = {};
+
+		for (int32_t kernelColumn = -radius; kernelColumn <= radius; kernelColumn++)
+		{
+			float kernelValue = kernel[kernelColumn + radius];
+			int32_t kernelSamplePixelIndex = pixelIndex + kernelColumn;
+
+			// If the kernel tries to process a pixel outside of the image, give it the value of the closest valid pixel.
+			if (kernelSamplePixelIndex < 0)
+			{
+				kernelSamplePixelIndex = 0;
+			}
+			else if (kernelSamplePixelIndex >= length)
+			{
+				kernelSamplePixelIndex = length - 1;
+			}
+
+			pixel.x += (float)pixels[kernelSamplePixelIndex].x * kernelValue;
+			pixel.y += (float)pixels[kernelSamplePixelIndex].y * kernelValue;
+			pixel.z += (float)pixels[kernelSamplePixelIndex].z * kernelValue;
+			pixel.w += (float)pixels[kernelSamplePixelIndex].w * kernelValue;
+		}
+
+		newPixels[pixelIndex].x = (uint8_t)ceil(pixel.x);
+		newPixels[pixelIndex].y = (uint8_t)ceil(pixel.y);
+		newPixels[pixelIndex].z = (uint8_t)ceil(pixel.z);
+		newPixels[pixelIndex].w = (uint8_t)ceil(pixel.w);
+	}
+
+	// Apply a 1D kernel in the vertical direction to all pixels.
+	for (size_t pixelIndex = 0; pixelIndex < length; pixelIndex++)
+	{
+		Vec4f pixel = {};
+		for (int32_t kernelRow = -radius; kernelRow <= radius; kernelRow++)
+		{
+			float kernelValue = kernel[kernelRow + radius];
+			int32_t kernelSamplePixelIndex = pixelIndex + (width * kernelRow);
+
+			// If the kernel tries to process a pixel outside of the image, give it the value of the closest valid pixel.
+			if (kernelSamplePixelIndex < 0)
+			{
+				kernelSamplePixelIndex = 0;
+			}
+			else if (kernelSamplePixelIndex >= length)
+			{
+				kernelSamplePixelIndex = length - 1;
+			}
+
+			pixel.x += (float)newPixels[kernelSamplePixelIndex].x * kernelValue;
+			pixel.y += (float)newPixels[kernelSamplePixelIndex].y * kernelValue;
+			pixel.z += (float)newPixels[kernelSamplePixelIndex].z * kernelValue;
+			pixel.w += (float)newPixels[kernelSamplePixelIndex].w * kernelValue;
+		}
+
+		newPixels[pixelIndex].x = (uint8_t)ceil(pixel.x);
+		newPixels[pixelIndex].y = (uint8_t)ceil(pixel.y);
+		newPixels[pixelIndex].z = (uint8_t)ceil(pixel.z);
+		newPixels[pixelIndex].w = (uint8_t)ceil(pixel.w);
 	}
 
 	return newPixels;
@@ -90,6 +171,36 @@ std::vector<std::vector<float>> Effects::GetGaussianMatrix(const int32_t radius,
 		{
 			kernel[x][y] /= sum;
 		}
+	}
+
+	return kernel;
+}
+
+std::vector<float> Effects::Get1DMatrix(const int32_t radius, const float sigma)
+{
+	// Kernel width is a function of the radius passed in.
+	// But we always want a width of at least 3, and width should be odd so there is always a center pixel.
+	size_t kernelWidth = std::max(((2 * radius) + 1), 3);
+
+	// Calculate Gaussian values for the kernel matrix.
+	std::vector<float> kernel(kernelWidth);
+	float sum = 0.0f;
+	for (int32_t i = -radius; i <= radius; i++)
+	{
+		float exponentNumerator = (float)(i * i);
+		float exponentDenominator = 2.0f * (sigma * sigma);
+
+		float eExpression = exp(-exponentNumerator / exponentDenominator);
+		float kernelValue = eExpression / sqrt(2.0f * (float)M_PI * sigma);
+
+		kernel[i + radius] = kernelValue;
+		sum += kernelValue;
+	}
+
+	// Normalize kernel values so they sum to 1.0.
+	for (auto& value : kernel)
+	{
+		value /= sum;
 	}
 
 	return kernel;
--- src/public/Effects.h
+++ src/public/Effects.h
@@ -19,6 +19,15 @@ public:
 	*/
 	static std::shared_ptr<Vec4[]> const GaussianBlur(const std::shared_ptr<Vec4[]>& pixels, const size_t width, const size_t height, float blurAmount);
 
+	/**
+	* Applies a Gaussian Blur effect to the given pixels.
+	* @param pixels The pixel data to modify.
+	* @param width The width of the pixel data.
+	* @param height The height of the pixel data.
+	* @param blurAmount Value of 0-1 inclusive. Higher value gives stronger blur effect.
+	*/
+	static std::shared_ptr<Vec4[]> const GaussianBlurSeparate(const std::shared_ptr<Vec4[]>& pixels, const size_t width, const size_t height, float blurAmount);
+
 private:
 
 	/**
@@ -38,4 +47,12 @@ private:
     * @return The normalized Gaussian matrix.
     */
 	static std::vector<std::vector<float>> GetGaussianMatrix(const int32_t radius, const float sigma);
+
+	/**
+	* Creates a normalized 1D Gaussian matrix of values.
+	* @param radius The radius of the kernel. Higher value gives stronger blurring effect.
+	* @param sigma The standard deviation to use for the kernel. Higher value gives stronger blurring effect.
+	* @return The normalized Gaussian matrix.
+	*/
+	static std::vector<float> Get1DMatrix(const int32_t radius, const float sigma);
 };
